0b689e68d343705297f3ade9d307abce22580984
Justin Meiners <justin.meiners@gmail.com>
2021-04-01 20:48:29 -0600

    lowercase sections and formatting

--- paper/code/free-rank.lisp
+++ paper/code/free-rank.lisp
@@ -4,7 +4,6 @@
             (- (aref word (- end 1))))
          (funcall memo (+ start 1) (- end 1))
          (+ 1 (- end start)))
-
     (loop for k from (+ start 1) below end minimize 
           (+ (funcall memo start k)
              (funcall memo k end)))))
@@ -17,17 +16,13 @@
                              :initial-element 0))
          (memo-lookup (lambda (start end)
                         (aref matrix start (- end 1))))) 
-
     (loop for i from 0 below N do
           (setf (aref matrix i i) 1))
-
     (loop for length from 2 to N do
           (do ((i 0 (+ i 1)))
               ((> (+ i length) N) nil)
-
               (let ((j (+ i (- length 1))))
                 (setf (aref matrix i j)
                       (rank-rule word i (+ j 1) memo-lookup)))))
-
     (values (aref matrix 0 (- N 1)) matrix)))
 
